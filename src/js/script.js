5a358f4718867c21a260ac286a2a9df68dd94a5a
Magdalena Dabrowska <mmdabrowska4@gmail.com>
2020-09-08 17:40:21 +0200

    add html and css code for main page

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// const hamburger = document.querySelector('.hamburger');
+// const navigation = document.querySelector('.navigation');
+
+// const clickHandler = function() {
+//   hamburger.classList.toggle('hamburger--active');
+//   navigation.classList.toggle('navigation--active');
+// };
+
+// hamburger.addEventListener('click', clickHandler);
